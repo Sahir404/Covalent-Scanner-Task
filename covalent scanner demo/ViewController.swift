a0d6247a41cb51bf8f0b9bdd5fc9b54a2b025faa
Syed Sahir Shah <60597425+Sahir404@users.noreply.github.com>
2024-10-21 22:13:42 +0500

    Initial Commit

---
+++ covalent scanner demo/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  covalent scanner demo
+//
+//  Created by MAC on 21/10/2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ covalent scanner demoTests/covalent_scanner_demoTests.swift
@@ -0,0 +1,17 @@
+//
+//  covalent_scanner_demoTests.swift
+//  covalent scanner demoTests
+//
+//  Created by MAC on 21/10/2024.
+//
+
+import Testing
+@testable import covalent_scanner_demo
+
+struct covalent_scanner_demoTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
